30aea1914377f9fe55791c5b59e17ed4a528dbed
GordinhoKungFu <163547370+GordinhoKungFu@users.noreply.github.com>
2025-05-15 12:20:12 -0300

    Add files via upload

---
+++ GREENGYM/index.php
@@ -0,0 +1,19 @@
+<?php
+// Receber os dados do formulário
+$dados filter_input_array(INPUT POST, FILTER DEFAULT);
+// Acessar o If quando o usuário clicar no botão acessar do formulário
+if(lempty($dados['SendLogin'])){ 
+var_dump($dados);
+}
+
+?>  
+
+<!-- inicio do formulário de login -->
+ <form method="POST" action="">
+    <label>Usuário: </label>
+    <input type="text" name="usuario" placeholder="Digite o usuário"><br><br>
+
+    <label>Senha: </label>
+    <input type="password" name="senha_usuario" placeholder="Digite a senha"><br><br>
+
+</form>
